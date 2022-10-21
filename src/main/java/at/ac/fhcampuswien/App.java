f675ea007cdedc2352a01b625e28a48c97234fdd
BroingerC <christina.broinger@stud.fh-campuswien.ac.at>
2022-10-21 20:28:06 +0200

    one Month Calendar finished

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -13,4 +13,35 @@ public class App {
         // print their results
         // etc.
     }
+
+    public static void oneMonthCalendar(int numberDay, int startingDay) {
+
+        int line = 0;
+        int row = 0;
+
+        for (int j = 1; j <= 7; j++){
+            if (j == startingDay){
+                for (int i = 1; i <= numberDay; i++){
+                    if (i <= 9){
+                        System.out.print(" " + i + " ");
+                    } else {
+                        System.out.print(i + " ");
+                    }
+                    line++;
+                    if (line % 7 == 0){
+                        if (row <= 4){
+                            System.out.println("");}
+                        row++;
+                    }
+                }
+            } else if (line < startingDay){
+                System.out.print("   ");
+                line++;
+            }
+            if (numberDay <= 2){
+                System.out.print("");}
+        }
+        if (line % 7 != 0){
+            System.out.println();}
+    }
 }
